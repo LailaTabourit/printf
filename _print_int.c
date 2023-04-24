f2c3e5c1bf417de677916009e2ba76b5906f9b9a
l11EG4 <zakariaberahmi@gmail.com>
2023-04-24 14:38:58 -0700

    task1

--- _print_int.c
+++ _print_int.c
@@ -14,13 +14,21 @@ int _print_int(int n)
 	if (n < 0)
 	{
 		cmp += _putchar('-');
-		n = -n;
+		if (n == -2147483648)
+		{
+			cmp += _putchar('2');
+			n %= 1000000000;
+		}
+		cmp += _print_int(-n):
 	}
 
-	if (n / 10)
+	else if (n >= 0 && n <= 9)
+		cmp += _putchar(n + '0');
+	else
+	{
 		cmp += _print_int(n / 10);
-
-	cmp += _putchar((n % 10) + '0');
+		cmp += _print_int(n % 10);
+	}
 	return (cmp);
 }
 
