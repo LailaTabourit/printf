6a5d5485fa2b6d5c3946918816529931e40a1f25
laila <lailatabourit@gmail.com>
2023-04-24 05:40:50 -0700

    ALX_tasks

--- main.h
+++ main.h
@@ -15,7 +15,6 @@
 #define BUFF_SIZE 1024
 
 /* --------------Prototype--------------------*/
-int _printf(const char *format, ...);
 int _check(va_list list, char c);
 int _putchar(char c);
 int _strlen(char *s);
@@ -23,5 +22,6 @@ int _print_str(char *str);
 int _print_bin(unsigned int n);
 int _print_unsigned_int(unsigned int n, int base, char *digit);
 int _print_int(int n);
-
+int _print_string(char *s, char *x, char *X);
+int _printf(const char *format, ...);
 #endif
