77bad1c2afbd29eacf3c35fe99b65e55e3bec31a
laila <lailatabourit@gmail.com>
2023-04-25 12:33:47 -0700

    ALX_tasks

--- _printf.c
+++ _printf.c
@@ -17,9 +17,9 @@ int _printf(const char *format, ...)
 
 	if (format == NULL)
 		return (-1);
-	if (!format || (format[0] == '%' && !format[i]))
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-	if (format[0] == '%' && format[i] == ' ' && !format[2])
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
 	while (format[i] != '\0')
 	{
